e1e22379de08d5479971d43abf58c3834a35dba6
IlonaMaltseva <maltseva32@gmail.com>
2024-01-02 16:30:43 +0200

    fix modal

--- markup/assets/js/all-src.js
+++ markup/assets/js/all-src.js
@@ -1,5 +1,4 @@
 // @prepros-append vendors/picturefill.js
 // @prepros-append vendors/swiper-bundle.min.js
 // @prepros-append vendors/jquery.fancybox.min.js
-// @prepros-append vendors/iziModal.min.js
 // @prepros-append jquery.main.js
